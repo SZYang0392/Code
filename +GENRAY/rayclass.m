72de2727df837635e54cd13179b616d33d6bf329
SZYang0392 <SZYang0392@163.com>
2026-06-22 11:23:05 +0800

    2026.6.22 : Modified GENRAY.rayclass to avoid error caused by NaN values of genrayout.wnpar(1, :).

--- +GENRAY/rayclass.m
+++ +GENRAY/rayclass.m
@@ -1,21 +1,44 @@
 function genrayout = rayclass(genrayout)
 % Classify rays with Npar and launch position
-    % Npar
+
+    % -------- Npar -------- %
     Npar = genrayout.wnpar(1, :);
-    genrayout.NNpar = 0;
-    genrayout.NparErr = 1e-5;
-    genrayout.Npar = uniquetol(Npar, genrayout.NparErr);
-    genrayout.NNpar = numel(genrayout.Npar);
-    genrayout.NparClass = discretize(Npar, [genrayout.Npar, Inf]);
-    genrayout.NparNum = histcounts(genrayout.NparClass, genrayout.NNpar);
-    % Theta
+    % Process only finite values (exclude NaN and Inf)
+    Npar_finite = Npar(isfinite(Npar));
+    
+    if isempty(Npar_finite)
+        % All values are NaN or Inf: no valid classification
+        genrayout.NNpar = 0;
+        genrayout.Npar = [];
+        genrayout.NparClass = NaN(size(Npar));
+        genrayout.NparNum = [];
+    else
+        genrayout.NparErr = 1e-5;   % Tolerance as originally set
+        genrayout.Npar = uniquetol(Npar_finite, genrayout.NparErr);
+        genrayout.NNpar = numel(genrayout.Npar);
+        % Classify original Npar (including NaN); NaN yields NaN in output
+        genrayout.NparClass = discretize(Npar, [genrayout.Npar, Inf]);
+        % Count per class; histcounts automatically ignores NaN
+        genrayout.NparNum = histcounts(genrayout.NparClass, genrayout.NNpar);
+    end
+
+    % -------- Theta -------- %
     Theta = genrayout.w_theta_pol(1, :);
-    genrayout.Ntheta = 0;
-    genrayout.thetaErr = 1e-5;
-    genrayout.theta = uniquetol(Theta, genrayout.thetaErr);
-    genrayout.Ntheta = numel(genrayout.theta);
-    genrayout.thetaClass = discretize(Theta, [genrayout.theta, Inf]);
-    genrayout.thetaNum = histcounts(genrayout.thetaClass, genrayout.Ntheta);
-    % Launch position
+    Theta_finite = Theta(isfinite(Theta));
+    
+    if isempty(Theta_finite)
+        genrayout.Ntheta = 0;
+        genrayout.theta = [];
+        genrayout.thetaClass = NaN(size(Theta));
+        genrayout.thetaNum = [];
+    else
+        genrayout.thetaErr = 1e-5;
+        genrayout.theta = uniquetol(Theta_finite, genrayout.thetaErr);
+        genrayout.Ntheta = numel(genrayout.theta);
+        genrayout.thetaClass = discretize(Theta, [genrayout.theta, Inf]);
+        genrayout.thetaNum = histcounts(genrayout.thetaClass, genrayout.Ntheta);
+    end
+
+    % Number of launch positions (keeps original logic: equals number of unique angles)
     genrayout.Nlaunch = numel(genrayout.theta);
 end
